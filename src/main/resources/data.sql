bb04be57ffae49905606baf4a7710763b531ff02
Abdisharif107 <143189472+Abdisharif107@users.noreply.github.com>
2024-05-09 13:23:29 +0200

    Init4

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO Billett (film,antall,fornavn,etternavn,adresse,mobilnummer,epost)
+VALUES ('Dune', '1', 'Abdi', 'Sharif', 'Osloveien 34', '98350047', 'abdi@mail.com');
+
+INSERT INTO Billett (film,antall,fornavn,etternavn,adresse,mobilnummer,epost)
+VALUES ('Dune', '1', 'Neymar Jr', 'Da Silva', 'Osloveien 34', '98350047', 'abdi@mail.com');
+
